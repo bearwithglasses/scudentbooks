d29864a6c24bd591f0f3e18931ea6ef1abba7432
alchemistgod <winstondchang@yahoo.com>
2017-02-07 04:42:32 -0800

    Added database folder in backend folder and database tables and updated change log

---
+++ BackEnd/database/createtables.sql
@@ -0,0 +1,85 @@
+--User info
+CREATE TABLE UserInfo(
+	userid int NOT NULL AUTO_INCREMENT,
+	username varchar(50) NOT NULL UNIQUE,
+	password varchar(50) NOT NULL,
+	firstName varchar(50) NOT NULL,
+	middleName varchar(50),
+	lastName varchar(50) NOT NULL,
+	emailAddress varchar(254) NOT NULL,
+	phoneNumber int,
+	major1 varchar(50) NOT NULL,
+	major2 varchar(50),
+	major3 varchar(50),
+	minor1 varchar(50),
+	minor2 varchar(50),
+	minor3 varchar(50),
+	year varchar(10),
+	location varchar(50),
+	CHECK (year in ('freshman','sophomore','junior','senior','graduate')),
+	PRIMARY KEY(userid)
+);
+
+--Book posts
+CREATE TABLE BookPost(
+	bookid int NOT NULL AUTO_INCREMENT,
+	userid int NOT NULL AUTO_INCREMENT,
+	title varchar(100) NOT NULL,
+	author text NOT NULL,
+	edition smallint,
+	purpose varchar(4) NOT NULL,
+	price decimal(5,2),
+	isbn int,
+	major varchar(50),
+	courseNumber smallint,
+	professor varchar(50),
+	postDate date DEFAULT GETDATE(),
+	condition varchar(5),
+	status varchar(20) DEFAULT 'available' NOT NULL,
+	CHECK (purpose in ('buy','swap')),
+	CHECK (condition in ('new','good','bad')),
+	CHECK (status in ('available','sale pending','unavailable')),
+	PRIMARY KEY(bookid),
+	FOREIGN KEY(userid) REFERENCES UserInfo
+);
+
+--Book post description
+CREATE TABLE BookDescription(
+	bookid int NOT NULL AUTO_INCREMENT,
+	description text,
+	FOREIGN KEY(bookid) REFERENCES BookPost
+);
+
+--Book post photos
+CREATE TABLE BookPhoto(
+	bookid int NOT NULL AUTO_INCREMENT,
+	photo1 image NOT NULL,
+	photo2 image,
+	photo3 image,
+	FOREIGN KEY(bookid) REFERENCES BookPost
+);
+
+--Email message
+CREATE TABLE Message(
+	messageid int NOT NULL AUTO_INCREMENT,
+	creator int NOT NULL,
+	recipient int NOT NULL,
+	subject varchar(100),
+	body text,
+	messageDate date DEFAULT GETDATE(),
+	PRIMARY KEY(messageid),
+	FOREIGN KEY(creator) REFERENCES UserInfo(userid),
+	FOREIGN KEY(recipient) REFERENCES UserInfo(userid)
+);
+
+--Email message reply
+CREATE TABLE MessageReply(
+	replyid int NOT NULL AUTO_INCREMENT,
+	replyUserid int NOT NULL,
+	replyMessageid int NOT NULL,
+	reply text,
+	replyDate date DEFAULT GETDATE(),
+	PRIMARY KEY(replyid),
+	FOREIGN KEY(replyUserid) REFERENCES UserInfo(userid),
+	FOREIGN KEY(replyMessageid) REFERENCES Message(messageid)
+);
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,15 +3,24 @@ All notable changes to this project will be documented in this file.
 
 2/6/2017
 	
-	Renee: Added
-		- Created FrontEnd folder 
-		- Added demo pages of website pages: homepage.html, booklisting.html, and userprofile.html with corresponding CSS files and JS file
+	Renee: 
+		- Added
+			- Created FrontEnd folder 
+			- Added demo pages of website pages: homepage.html, booklisting.html, and userprofile.html with corresponding CSS files and JS file
+		- FrontEnd Updates
+			- Basic styling
+			- Made pages responsive for mobile dimensions
+			- Message box pop-up modal appears on user profiles and book listings after clicking "Send a Message" button
 		
-	Christina: Added
-		- Created BackEnd folder
-		- Uploaded current code for creating book database, adding books, and searching for books. Will be changed accordingly.
-		
-	Renee: FrontEnd Updates
-		- Basic styling
-		- Made pages responsive for mobile dimensions
-		- Message box pop-up modal appears on user profiles and book listings after clicking "Send a Message" button
+	Christina: 
+		- Added
+			- Created BackEnd folder
+			- Uploaded current code for creating book database, adding books, and searching for books. Will be changed accordingly.
+
+	Winston: 
+		- Added
+			- Created database folder in BackEnd folder
+			- Added database tables: createtables.sql
+		- BackEnd Updates
+			- createtables.sql
+				- Modified database tables and added email message table. Will be changed accordingly
