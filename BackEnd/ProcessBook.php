47e87be482eec9eeb09400b47c901889eca3891a
christinaciardella <stinalovespandas@gmail.com>
2017-03-17 15:52:10 -0700

    updated credentials, functions, and made new sql file
    Not sure if php code is still relevant… may need to change more to sql.
    Will also add more to PopulateDatabase.sql but I just made the file for
    now.

--- BackEnd/ProcessBook.php
+++ BackEnd/ProcessBook.php
@@ -1,24 +1,28 @@
 <?php
-include ("RegBooksFnc.php");
+include ("DBTablesFnc.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	//$input_comment = $_POST['mComment'];
-	$input_deptName = $_POST['book_deptName'];
-	$input_classNo = $_POST['book_classNo'];
+	$input_bookid = $_POST['book_id'];
+	$input_userid = $_POST['book_userid'];
 	$input_title = $_POST['book_title'];
 	$input_author = $_POST['book_author'];
 	$input_edition = $_POST['book_edition'];
-	$input_condition = $_POST['book_condition'];
+	$input_purpose = $_POST['book_purpose'];
 	$input_price = $_POST['book_price'];
-	$input_sellerName = $_POST['seller_name'];
-	$input_email = $_POST['seller_email'];
-	$input_phone = $_POST['seller_phone'];
+	$input_isbn = $_POST['book_isbn'];
+	$input_major = $_POST['book_major'];
+	$input_courseNo = $_POST['book_courseno'];
+	$input_professor = $_POST['book_prof'];
+	$input_postDate = $_POST['book_date'];
+	$input_condition = $_POST['book_condition'];
+	$input_status = $_POST['book_status'];
 	
-	echo "I'm here " . $input_title . " </br>";
+	//echo "I'm here " . $input_title . " </br>";
 	//$mDate = date("m/d/y - H:i");
 	//$mcode = getBookCount()+1;
 	//echo $mcode;
-	insertFormValues($input_deptName,$input_classNo,$input_title,$input_author,$input_edition,
-	$input_condition,$input_price,$input_sellerName,$input_email,$input_phone);
+	insertFormValues($input_bookid,$input_userid,$input_title,$input_author,$input_edition,
+	$input_purpose,$input_price,$input_isbn,$input_major,$input_courseNo,$input_professor,
+	$input_postDate,$input_condition,$input_status);
 	
 	header('Location: thanks.php');
 }
