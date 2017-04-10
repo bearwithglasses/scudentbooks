ed48f7b66315162182e22d793d7c70cafd0a83f7
bearwithglasses <misterbillyflynn@gmail.com>
2017-04-10 11:30:55 -0700

    Revert "updated CSS/searchpage/addbook pages in demofolder"
    This reverts commit 1c29d94de73ed933007a4d65bed450f50a4b0481.

---
+++ FrontEnd/demopages/thanks.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+
+</head>
+<body>
+<p>Thanks. Your book has been added to the database successfully.<p>
+<p class="refLink"><a href="addBook.html">Click here to add another book</a></p>
+</body>
+</html>
